be0ea584ee317ae8aa0d47a736af67a46cbe09ff
Arcayn <siriisepic@gmail.com>
2019-03-10 21:18:18 +0000

    Version 1 - Overhauled commands, complete code refactor, split across files Still pretty unreliable though

---
+++ src/commands.js
@@ -0,0 +1,255 @@
+//TODO: Add 'fairshare' command
+//TODO: Add dnd autolevelling
+Discord = require("discord.js");
+
+
+const LEVELS = {
+}
+
+module.exports = {
+	aliases: {
+		"earn": ['earn', 'gain', 'obtain', 'recieve'],
+		"spend": ['spend', 'lose', 'pay'],
+		"communism": ['communism', 'pool'],
+		"capitalism": ['capitalism', 'take', 'unpool'],
+		"balance": ['balance', 'bal', 'balances', 'bals'],
+		"transactions": ['trans', 'log', 'transactions'],
+		"levelup": ['levelup', 'lvl'],
+		"resetxp": ['resetxp', 'xpreset', ],
+		"addxp": ['addxp', 'xp'],
+		"party": ['party', 'people', 'levels', 'xp']
+	},
+	earn: {
+		isAdminOnly: false,
+		action: earnCommand
+	},
+	spend: {
+		isAdminOnly: false,
+		action: spendCommand
+	},
+	communism: {
+		isAdminOnly: false,
+		action: communismCommand
+	},
+	capitalism: {
+		isAdminOnly: false,
+		action: capitalismCommand
+	},
+	balance: {
+		isAdminOnly: false,
+		action: balanceCommand
+	},
+	transactions: {
+		isAdminOnly: false,
+		action: transactionsCommand
+	},
+	levelup: {
+		isAdminOnly: false,
+		action: levelupCommand
+	},
+	resetxp: {
+		isAdminOnly: false,
+		action: resetxpCommand
+	},
+	addxp: {
+		isAdminOnly: false,
+		action: addxpCommand
+	},
+	party: {
+		isAdminOnly: false,
+		action: partyCommand
+	}
+}
+
+var people = [];
+
+async function checkTarget(person, storage) {
+	if (!people.includes(person)) {
+		people.push(person);
+		storage.setItem('ADMIN_PEOPLE', people);
+		await storage.setItem(person, {'gp': 0,
+			'level': 1,
+			'xp': 0
+		});
+	}
+}
+
+async function earnCommand(storage, globalParams, params, person, isAdmin, msg, protoPeople) {
+	people = protoPeople;
+	var balInc = parseInt(params.shift());
+    if (isAdmin && params.length) { person = params.shift(); await checkTarget(person, storage); }
+    var personObj = await storage.getItem(person);
+	personObj.gp = personObj.gp + balInc;
+	
+	var personName = (isAdmin) ? person + "'s" : "Your";
+	
+	msg.reply(personName + ' balance is now at: **'+ (personObj.gp).toString() + 'gp**');
+	var reason = (params.length) ? params.join(" ") : "no good reason";
+	
+    storage.setItem(person, personObj);
+	
+	return person + " earned **" + (balInc).toString() + "gp** for " + reason;
+}
+
+async function spendCommand(storage, globalParams, params, person, isAdmin, msg, protoPeople) {
+	people = protoPeople;
+	var balDec = parseInt(params.shift());
+    if (isAdmin && params.length) { person = params.shift(); await checkTarget(person, storage); }
+    var personObj = await storage.getItem(person);
+	if (personObj.gp - balDec < 0) {
+		msg.reply('**Come back when you get some money buddy!**');
+		return false;
+	}
+	
+	personObj.gp = personObj.gp - balDec;
+	
+	var personName = (isAdmin) ? person + "'s" : "Your";
+	
+	msg.reply(personName + ' balance is now at: **'+ (personObj.gp).toString() + 'gp**');
+	var reason = (params.length) ? params.join(" ") : "nothing in particular";
+	
+    storage.setItem(person, personObj);
+	
+	return person + " spent **" + (balDec).toString() + "gp** on " + reason;
+}
+
+async function communismCommand(storage, globalParams, params, person, isAdmin, msg, protoPeople) {
+	people = protoPeople;
+	var balDec = parseInt(params.shift());
+    if (isAdmin && params.length) { person = params.shift(); await checkTarget(person, storage); }
+    var personObj = await storage.getItem(person);
+	var communalBalance = await storage.getItem('COMMUNAL')
+	
+	if (personObj.gp - balDec < 0) {
+		msg.reply('**Come back when you get some money buddy!**');
+		return false;
+	}
+	
+	personObj.gp = personObj.gp - balDec;
+	
+	var personName = (isAdmin) ? person + "'s" : "Your";
+	
+	msg.reply(personName + ' balance is now at: **'+ (personObj.gp).toString() + 'gp**, there is **' + (communalBalance + balDec).toString() + 'gp** in the communal pool');
+	
+    storage.setItem(person, personObj);
+	storage.setItem('COMMUNAL', communalBalance + balDec);
+	
+	return false;
+}
+
+async function capitalismCommand(storage, globalParams, params, person, isAdmin, msg, protoPeople) {
+	people = protoPeople;
+	var balInc = parseInt(params.shift());
+    if (isAdmin && params.length) { person = params.shift(); await checkTarget(person, storage); }
+    var personObj = await storage.getItem(person);
+	var communalBalance = await storage.getItem('COMMUNAL')
+	
+	if (communalBalance - balInc < 0) {
+		msg.reply('**Can\'t take that much!**');
+		return false;
+	}
+	
+	personObj.gp = personObj.gp + balInc;
+	
+	var personName = (isAdmin) ? person + "'s" : "Your";
+	
+	msg.reply(personName + ' balance is now at: **'+ (personObj.gp).toString() + 'gp**, there is **' + (communalBalance - balInc).toString() + 'gp** in the communal pool');
+	
+    storage.setItem(person, personObj);
+	storage.setItem('COMMUNAL', communalBalance - balInc);
+	
+	return false;
+}
+
+async function balanceCommand(storage, globalParams, params, person, isAdmin, msg, protoPeople) {
+	people = protoPeople;
+    var embed = new Discord.RichEmbed().setTitle("Current Balances:").setColor(0x00AE86)
+	
+    for (var i = 0; i < people.length; i++) {
+       var personObj = await storage.getItem(people[i]);
+       embed.addField(people[i], personObj.gp.toString(), true);
+    }
+	
+    var communalBal = await storage.getItem('COMMUNAL');
+    embed.addField('***Communal Pool***', communalBal.toString(), true);
+    msg.channel.send({embed});
+	
+	return false;
+}
+
+async function transactionsCommand(storage, globalParams, params, person, isAdmin, msg, protoPeople, transactions) {
+	people = protoPeople;
+    var embed = new Discord.RichEmbed().setTitle("Recent transactions:").setColor(0x00AE86)
+    var text = "";
+	
+    for (var i = 0; i < transactions.length; i++) {
+       text += "\n" + transactions[i];
+    }
+	
+    embed.setDescription(text);
+    msg.channel.send({embed});
+	
+	return false;
+}
+  
+async function levelupCommand(storage, globalParams, params, person, isAdmin, msg, protoPeople) {
+	people = protoPeople;
+	var inc = 1;
+	if (isAdmin) {
+		if (params.length) { inc = parseInt(params.shift()); }
+		if (params.length) { person = params.shift(); await checkTarget(person, storage); }
+	}
+
+    var personObj = await storage.getItem(person);
+	
+	personObj.level += inc;
+    storage.setItem(person, personObj);
+	
+	var personName = (isAdmin) ? person + " is" : "You are";
+	
+    msg.reply('Congratulations! ' + personName + ' now level **'+ (personObj.level).toString() + '**!');
+	return false;
+}
+
+async function addxpCommand(storage, globalParams, params, person, isAdmin, msg, protoPeople) {
+	people = protoPeople;
+	var inc = parseInt(params.shift());
+	if (isAdmin) {
+		if (params.length) { person = params.shift(); await checkTarget(person, storage); }
+	}
+
+    var personObj = await storage.getItem(person);
+	
+	personObj.xp += inc;
+    storage.setItem(person, personObj);
+	
+	var personName = (isAdmin) ? person + " now has" : "You now have";
+    msg.reply('Congratulations! ' + personName + ' **'+ (personObj.xp).toString() + 'xp**!');
+	return false;
+}
+
+async function resetxpCommand(storage, globalParams, params, person, isAdmin, msg, protoPeople) {
+	people = protoPeople;
+	if (isAdmin) {
+		if (params.length) { person = params.shift(); await checkTarget(person, storage); }
+	}
+
+    var personObj = await storage.getItem(person);
+	
+	personObj.xp = 0;
+    storage.setItem(person, personObj);
+    msg.reply('Reset xp to 0');
+	return false;
+}
+
+async function partyCommand(storage, globalParams, params, person, isAdmin, msg, protoPeople) {
+	people = protoPeople;
+	var embed = new Discord.RichEmbed().setTitle("Current Levels:").setColor(0x00AE86);
+	
+    for (var i = 0; i < people.length; i++) {
+       var personObj = await storage.getItem(people[i]);
+       embed.addField(people[i], '**' + personObj.level.toString() + '** ' + personObj.xp.toString(), true);
+    }
+    msg.channel.send({embed});
+	return false;
+}
--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
 const Discord = require('discord.js');
 const storage = require('node-persist');
-const Params = require('./opt.js')
+const Params = require('./opt.js');
+const Commands = require('./commands.js');
 const client = new Discord.Client();
 
-//await storage.setItem('name','yourname')
-//console.log(await storage.getItem('name'));
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+const Priorities = {
+	'ERROR': 5,
+	'CRITICAL': 4,
+	'HIGH': 3,
+	'INFO': 2,
+	'LOG': 1
+}
 
 var people = [];
 var transactions = [];
@@ -27,150 +29,64 @@ async function setup() {
   if (typeof(communal) != 'number') {
     await storage.setItem('COMMUNAL', 0);
   }
-  //people.shift();
-  //transactions = [];
 }
 
 function addTrans(trans) {
   transactions.push(trans);
-  if (transactions.length > 15) {
+  if (transactions.length > Params.keptTransactions) {
     transactions.shift();
   }
   storage.setItem("ADMIN_TRANSACTIONS", transactions);
 }
 
+async function addNewPerson(person) {
+	people.push(person);
+    storage.setItem('ADMIN_PEOPLE', people);
+    await storage.setItem(person, {'gp': 0,
+		'level': 1,
+		'xp': 0
+		});
+}
+
+async function executeCommand(command, messageWords, person, admin, msg) {
+	if (!Commands[command].adminOnly || admin) {
+		var retVal = await Commands[command].action(storage, Params, messageWords, person, admin, msg, people, transactions);
+		if (retVal) {
+			addTrans(retVal);
+		}
+	}
+}
+
+var sendConsoleMsg = (msg, priority=Priorities.LOG) => {
+	if (priority > 4 - Params.verbosity) { console.log(msg); }
+}
+
 setup();
 
+client.on('ready', () => {
+  sendConsoleMsg(`Logged in as ${client.user.tag}. Listening...`, Priorities.CRITICAL);
+});
+
 client.on('message', async (msg) => {
-  var wordz = msg.content.split(" ");
-  if (msg.author.bot || msg.content.substr(0,1) != '!') { return; }
+  var messageWords = msg.content.split(" ");
+  if (msg.author.bot || messageWords[0] != '!econ') { return; }
+  messageWords.shift();
+  var sentCommand = messageWords.shift().toLowerCase();
   var person = msg.author.username;
+  
   if (!people.includes(person)) {
-    people.push(person);
-    storage.setItem('ADMIN_PEOPLE', people);
-    await storage.setItem(person, 0);
-    await storage.setItem(person + '_xp', 0);
-    await storage.setItem(person + '_level', 0);
+    await addNewPerson(person);
   }
-  var admin = (Params.adminUsernames.includes(person) && Params.adminChannels.includes(msg.channel.name)) ? true : false;
-
-  if (wordz[0] === '!earn') {
-    wordz.shift();
-    var inc = parseInt(wordz.shift());
-    console.log(inc);
-    if (admin) { person = wordz.shift(); }
-    console.log(person);
-    var curr = await storage.getItem(person);
-    storage.setItem(person, inc + curr);
-    msg.reply('Your balance is now at: **'+ (inc + curr).toString() + 'gp**');
-    var reason = wordz.join(" ");
-    addTrans(person + " earned **" + (inc).toString() + "gp** for " + reason);
-  }
-  else if (wordz[0] === '!spend') {
-    wordz.shift();
-    var dec = parseInt(wordz.shift());
-    if (admin) { person = wordz.shift(); }
-    var curr = await storage.getItem(person);
-    if ((curr - dec) < 0) {
-      msg.reply('**Come back when you have some money buddy!**');
-    }
-    else {
-      storage.setItem(person, curr - dec);
-      msg.reply('Your balance is now at: **'+ (curr - dec).toString() + 'gp**');
-      var reason = wordz.join(" ");
-      addTrans(person + " spent **" + (dec).toString() + "gp** on " + reason);
+  sendConsoleMsg("Recieved command: " + sentCommand, Priorities.LOG);
+  var admin = (Params.adminNames.includes(person) && Params.adminChannels.includes(msg.channel.name)) ? true : false;
+  for (var command in Commands.aliases) {
+    if (Commands.aliases.hasOwnProperty(command)) {
+        if (Commands.aliases[command].includes(sentCommand)) {
+			await executeCommand(command, messageWords, person, admin, msg);
+			break;
+		}
     }
   }
-  else if (wordz[0] === '!communism') {
-    var dec = parseInt(wordz[1]);
-    if (wordz[2] && admin) { person = wordz[2]; }
-    var curr = await storage.getItem(person);
-    var comm = await storage.getItem('COMMUNAL');
-    if ((curr - dec) < 0) {
-      msg.reply('**Come back when you have some money buddy!**');
-    }
-    else {
-      storage.setItem(person, curr - dec);
-      storage.setItem('COMMUNAL', comm + dec);
-      msg.reply('Your balance is now at: **'+ (curr - dec).toString() + 'gp**, there is **' + (comm + dec).toString() + 'gp** in the communal pool');
-    }
-
-  }
-  else if (wordz[0] === '!fairshare') {
-
-  }
-  else if (wordz[0] === '!balance') {
-    var embed = new Discord.RichEmbed().setTitle("Current Balances:").setColor(0x00AE86)
-    for (var i = 0; i < people.length; i++) {
-       var bal = await storage.getItem(people[i]);
-       embed.addField(people[i], bal.toString(), true);
-    }
-    var bal = await storage.getItem('COMMUNAL');
-    embed.addField('***Communal Pool***', bal.toString(), true);
-    msg.channel.send({embed});
-  }
-  else if (wordz[0] === '!transactions') {
-    var embed = new Discord.RichEmbed().setTitle("Recent transactions:").setColor(0x00AE86)
-    var text = "";
-    for (var i = 0; i < transactions.length; i++) {
-       text += "\n" + transactions[i];
-    }
-    embed.setDescription(text);
-    msg.channel.send({embed});
-  }
-  else if (wordz[0] === '!capitalism') {
-    var dec = parseInt(wordz[1]);
-    if (wordz[2] && admin) { person = wordz[2]; }
-    var curr = await storage.getItem(person);
-    var comm = await storage.getItem('COMMUNAL');
-    if ((comm - dec) < 0) {
-      msg.reply('**No Apple AirPods for you!**');
-    }
-    else {
-      storage.setItem(person, curr + dec);
-      storage.setItem('COMMUNAL', comm - dec);
-      msg.reply('Your balance is now at: **'+ (curr + dec).toString() + 'gp**, there is **' + (comm - dec).toString() + 'gp** in the communal pool');
-    }
-  }
-  else if (wordz[0] === '!levelup') {
-    if (wordz[2] && admin) { person = wordz[2]; }
-    var inc = 1;
-    if (wordz[1] && admin) {inc = parseInt(wordz[1]);}
-    var curr = await storage.getItem(person + '_level');
-    storage.setItem(person + '_level', curr + inc);
-    msg.reply('Congratulations! You are now level **'+ (curr + inc).toString() + '**');
-  }
-  else if (wordz[0] === '!addxp') {
-    if (wordz[2] && admin) { person = wordz[2]; }
-    var curr = await storage.getItem(person + '_xp');
-    var inc = wordz[1];
-    storage.setItem(person + '_xp', curr + inc);
-    msg.reply('Congratulations! You now have **'+ (curr + inc).toString() + '**xp');
-  }
-  else if (wordz[0] === '!resetxp') {
-   if (wordz[1] && admin) { person = wordz[1]; }
-    var curr = await storage.getItem(person + '_xp');
-    storage.setItem(person + '_xp', 0);
-    msg.reply('Reset your xp to 0');
-  }
-  else if (wordz[0] === '!party') {
-      var embed = new Discord.RichEmbed().setTitle("Current Levels:").setColor(0x00AE86)
-    for (var i = 0; i < people.length; i++) {
-       var lvl = await storage.getItem(people[i] + '_level');
-       var xp = await storage.getItem(people[i] + '_xp');
-       embed.addField(people[i], '**' + lvl.toString() + '** ' + xp.toString(), true);
-    }
-    msg.channel.send({embed});
-  }
-  else if (wordz[0] == "!addtarget" && admin) {
-    var person = wordz[1];
-    people.push(person);
-    storage.setItem('ADMIN_PEOPLE', people);
-    await storage.setItem(person, 0);
-    await storage.setItem(person + '_xp', 0);
-    await storage.setItem(person + '_level', 0);
-  }
-
 });
 
 client.login(Params.token);
